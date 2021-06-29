d55e6fc702846ac73389b27035b01a065b12f2e1
António Silva <35099204+Antonio-LinksPatrocinados@users.noreply.github.com>
2021-06-29 16:01:00 +0100

    Update trataki-for-woocommerce.php

--- trataki-for-woocommerce.php
+++ trataki-for-woocommerce.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Trataki for WooCommerce
- * Version:     0.1.8
+ * Version:     0.1.9
  * Plugin URI:  https://www.trataki.com
  * Description: Configure and export your WooCommerce products to Trataki marketpalce.
  * Author:      Trataki
